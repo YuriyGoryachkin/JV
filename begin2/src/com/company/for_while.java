10dae89f34eead852af25ca60ea5648fc6ccd5c4
I-msi <monguste@gmail.com>
2019-03-03 20:51:53 +0300

    modules

--- begin2/src/com/company/for_while.java
+++ begin2/src/com/company/for_while.java
@@ -1,4 +1,21 @@
 package com.company;
 
 public class for_while {
+    public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            System.out.println("i = " + i);
+        }
+        int i = 0;
+        do {
+            System.out.println("i = " + i);
+            i++;
+        }
+        while (i<10);
+        int i2 = 0;
+        while (i2<10){
+            System.out.println("i = " + i2);
+            i2++;
+        }
+
+    }
 }
---
+++ begin2/src/com/company/modules.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class modules {
+    public static void main(String[] args){
+        hello();
+        welcome();
+    }
+    static void hello(){
+        System.out.println("Hello!");
+    }
+    static void welcome(){
+        System.out.println("Welcome!");
+    }
+}
